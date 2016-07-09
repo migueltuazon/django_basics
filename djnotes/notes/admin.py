2b625849d2c8e31ca64912ed613df0ac62db49a6
Miguel03 <migueltuazon@yahoo.com>
2016-07-09 15:34:33 +0800

    Initial Commit

---
+++ djnotes/notes/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+# Register your models here.
+class NoteAdmin(admin.ModelAdmin)
+    list_display = (
+        'id',
+        'title',
+        'note',
+    )
+    search_fields = (
+        'title',
+        'note',
+    )
+
+admin.site.register(Note, NoteAdmin)
