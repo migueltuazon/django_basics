6bc0794f06e46459a7cc2938117e6c1d18d0fc37
Miguel03 <migueltuazon@yahoo.com>
2016-07-09 17:19:10 +0800

    Initial Commit v5.0

--- djnotes/notes/urls.py
+++ djnotes/notes/urls.py
@@ -13,10 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('notes.urls')),
+    url(r'^$', views.Index.as_view(), name='index'),
 ]
