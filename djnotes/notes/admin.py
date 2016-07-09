ed3902d2b2b3c24a665b6e3a2bccc91cfc02feab
Miguel03 <migueltuazon@yahoo.com>
2016-07-09 16:12:52 +0800

    Initial Commit v3.0

--- djnotes/notes/admin.py
+++ djnotes/notes/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-class NoteAdmin(admin.ModelAdmin)
+class NoteAdmin(admin.ModelAdmin):
     list_display = (
         'id',
         'title',
